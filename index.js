ed7522069a59ce1eb6bbe9074ba6cb4c92ea3140
shemarjoseph <shemarjoseph@yahoo.com>
2018-11-01 22:39:20 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,52 @@ const questions = [
 ]
 
 let question;
+
+const questionContainer = document.querySelector('.question-container');
+
+function appendQuestion(question) {
+  questionContainer.innerHTML = question.questionText;
+}
+
+function askQuestionThen(time) {
+  question = questions[0];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(question);
+    }, time)
+  });
+}
+
+function removeQuestion() {
+  return new Promise((resolve, reject) => {
+    questionContainer.innerHTML = '';
+    resolve(); 
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  questionContainer.innerHTML = question.questionText;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      questionContainer.innerHTML = '';
+      resolve();
+    }, time);
+  })
+}
+
+function trueAndFalseButtons() {
+  let trueBtn = document.querySelector('.green');
+  let falseBtn = document.querySelector('.red');
+  return [trueBtn, falseBtn];
+}
+
+function toggleTrueAndFalseButtons() {
+  const btns = document.getElementsByClassName('btn');
+  for (let btn of btns) {
+    btn.classList.toggle('hide');
+  } 
+}
+
+function displayQuestionOnClick() {
+  
+}
